2b7ab652f3c0455d00c1bc5cd0dcccd4cedb5d9f
Eli Perelman <eli@eliperelman.com>
2012-02-03 11:19:44 -0600

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,18 +1,20 @@
-(function( ns ) {
-	
-	ns && ns.utilities = {
-		
-		// Compare a value's internal [[Class]] property to a normalized type and check for equality.
-		// Makes type-checking across many value types much simpler.
-		// Example: is( [ ], 'array' ) | is( /value/, 'regexp' ) | is ( undefined, 'undefined' )
-		is: function ( value, type ) {
-			return type ===
-				// If the value is null or undefined compare directly to it:
-				value == null ? '' + value :
-					// Otherwise get the [[Class]] and compare to the relevant part of the value:
-					({ }).toString.call( value ).slice( 8, -1 ).toLowerCase();
-		}
+(function( global ) {
 
+	var is = function ( value, type ) {
+		return type ===
+			// If the value is null or undefined compare directly to it:
+			value == null ? '' + value :
+			// Otherwise get the [[Class]] and compare to the relevant part of the value:
+				({ }).toString.call( value ).slice( 8, -1 ).toLowerCase();
 	};
+	
+	// Thanks to @nathansmith for this one: https://gist.github.com/1027395
+	var parseInt = window.parseInt;
+	
+	if ( parseInt( '09' ) === 0 ) {
+		window.parseInt = function ( str, radix ) {
+			return parseInt( str, radix || 10 );
+		};
+	}
 
-})();
+})( this );
