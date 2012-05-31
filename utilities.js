cedafc6589e5f90d493b2052dae8185b618a1723
Eli Perelman <eli@eliperelman.com>
2012-05-30 19:35:36 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,22 +1,22 @@
 (function ( window ) {
 
-	var is = function ( value, type ) {
-		return type ===
-			// If the value is null or undefined compare directly to it:
-			value == null ?
-				'' + value :
-				// Otherwise get the [[Class]] and compare to the relevant part of the value:
-				({ }).toString.call( value ).slice( 8, -1 ).toLowerCase();
-	};
+    var is = function ( value, type ) {
+        return type ===
+            // If the value is null or undefined compare directly to it:
+            value == null ?
+                '' + value :
+                // Otherwise get the [[Class]] and compare to the relevant part of the value:
+                ({ }).toString.call( value ).slice( 8, -1 ).toLowerCase();
+    };
 	
-	// Thanks to @nathansmith for this one: https://gist.github.com/1027395
-	var parseInt = window.parseInt;
-	
-	if ( parseInt( '09' ) === 0 ) {
-		window.parseInt = function ( str, radix ) {
-			return parseInt( str, radix || 10 );
-		};
-	}
+    // Thanks to @nathansmith for this one: https://gist.github.com/1027395
+    var parseInt = window.parseInt;
+
+    if ( parseInt( '09' ) === 0 ) {
+        window.parseInt = function ( str, radix ) {
+            return parseInt( str, radix || 10 );
+        };
+    }
 
     // async functional helper
     // returns back a new version of a function
